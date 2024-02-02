1be7e1d33d7e4d2e8766fe8a23a6f4fac250cd03
apmoskevitz <andrew.moskevitz@gdbiw.com>
2024-02-02 12:17:39 -0500

    rename file

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ internal class Program
     {
         //access to static member through derived class with same name in different namespace
         Console.WriteLine(Base.StaticPropertyOnActualBase);
-        //code fix suggests this
+        //Simplify Member Access code fix suggests this
         Console.WriteLine(Core.Longer.Namespace.To.Make.Weirdness.Obvious.Base.StaticPropertyOnActualBase);
     }
 }
